5fc60c7642747a700b262cb4a9ec5bfe97879540
vplacido <valentinplacido25@gmail.com>
2020-04-26 23:18:44 -0400

    added authentication with sessions cookies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
         user = User.all     
         render json: user
     end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {user: user, jwt: token}
+        else
+            render json: {erros: user.errors.full_messages}, status : :not_acceptable
+        end
+    end
+
+    private 
+    def user_params
+        params.permit(:username, :password)
+    end
 end
